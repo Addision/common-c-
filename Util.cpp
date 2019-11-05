3d5660fc3d50d3274bd0099f30a160815fd4f818
laijia <laijia2008@126.com>
2019-11-05 15:58:14 +0800

    add some util function

--- Util.cpp
+++ Util.cpp
@@ -4,10 +4,11 @@
 #include <stdarg.h>
 #include <math.h>
 #include <string.h>
+#include <chrono>
 
 std::string TimeToDate(time_t t)
 {
-	struct tm* ptm = localtime(&t);
+	struct tm *ptm = localtime(&t);
 	char buf[32];
 	memset(buf, '\0', sizeof(buf));
 	strftime(buf, sizeof(buf), "%Y/%m/%d %H:%M:%S", ptm);
@@ -15,7 +16,7 @@ std::string TimeToDate(time_t t)
 	return str;
 }
 
-time_t DateToTime(const std::string& date)
+time_t DateToTime(const std::string &date)
 {
 	struct tm stm;
 	int year, month, day, hour, minute, second;
@@ -37,7 +38,7 @@ bool IsFloatEqual(float a, float b)
 	return ::fabs(a - b) < 0.0001;
 }
 
-std::string StringFormat(const char* fmt, ...)
+std::string StringFormat(const char *fmt, ...)
 {
 	va_list ap;
 	va_start(ap, fmt);
@@ -47,4 +48,41 @@ std::string StringFormat(const char* fmt, ...)
 	std::vsnprintf(buff.data(), buff.size(), fmt, ap);
 	va_end(ap);
 	return buff.data();
+}
+
+time_t GetTimeSeconds()
+{
+	auto now = std::chrono::system_clock::now();
+	auto now_epoch = std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch());
+	return (time_t)now_epoch.count();
+}
+time_t GetTimeMs()
+{
+	auto now = std::chrono::system_clock::now();
+	auto now_epoch = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch());
+	return (time_t)now_epoch.count();
+}
+
+time_t GetTimeMMs()
+{
+	auto now = std::chrono::system_clock::now();
+	auto now_epoch = std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch());
+	return (time_t)now_epoch.count();
+}
+
+std::string &StringTrim(std::string &s)
+{
+	if (s.empty())
+	{
+		return s;
+	}
+	s.erase(0, s.find_first_not_of(" "));
+	s.erase(s.find_last_not_of(" ") + 1);
+	return s;
+}
+
+int GetDigitDate(time_t t)
+{
+	struct tm *ptm = localtime(&t);
+	return (ptm->tm_year + 1900) * 10000 + (ptm->tm_mon + 1) * 100 + ptm->tm_mday;
 }
--- Util.h
+++ Util.h
@@ -3,8 +3,15 @@
 #include <time.h>
 #include <string>
 
-std::string TimeToDate(time_t t);
-time_t DateToTime(const std::string& date);
-bool IsFloatEqual(float a, float b);
 
+bool IsFloatEqual(float a, float b);
 std::string StringFormat(const char* fmt, ...);
+
+std::string TimeToDate(time_t t);
+time_t DateToTime(const std::string &date);
+int GetDigitDate(time_t t);
+time_t GetTimeSeconds();
+time_t GetTimeMs();
+time_t GetTimeMMs();
+
+std::string &StringTrim(std::string &s);
